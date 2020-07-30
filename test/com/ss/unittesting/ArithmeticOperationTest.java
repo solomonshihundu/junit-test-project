d39c8455dfdfe861f042f92b52881bc69d73fd07
Solo <shihundusolomon@gmail.com>
2020-07-30 17:27:04 +0300

    junit annotations

--- test/com/ss/unittesting/ArithmeticOperationTest.java
+++ test/com/ss/unittesting/ArithmeticOperationTest.java
@@ -1,11 +1,12 @@
 package com.ss.unittesting;
 
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
 
 public class ArithmeticOperationTest
 {
+
     ArithmeticOperation  arithmeticOperation = new ArithmeticOperation();
     /**
      * addOp(1,2,3)->6
@@ -18,6 +19,7 @@ public class ArithmeticOperationTest
          * check if result  equals expected value
          */
         assertEquals(6,arithmeticOperation.addOp(new int[]{1,2,3}));
+        System.out.println("TEST OF 3 NUMS");
 
 
     }
@@ -34,7 +36,35 @@ public class ArithmeticOperationTest
          * check if result  equals expected value
          */
         assertEquals(3,arithmeticOperation.addOp(new int[]{3}));
+        System.out.println("TEST OF 1 NUM");
 
 
     }
+
+    @BeforeClass
+    public static void beforeClass()
+    {
+        System.out.println("BEGINNING OF TESTNG");
+    }
+
+    @Before
+    public  void beforeTest()
+    {
+        System.out.println("BEFORE UNIT TEST");
+    }
+
+    @After
+    public  void afterTest()
+    {
+        System.out.println("AFTER UNIT TEST");
+    }
+
+    @AfterClass
+    public static void afterClass()
+    {
+        System.out.println("END OF TESTING");
+    }
+
+
 }
+
